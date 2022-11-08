08ff7ae6a32625f2ee031db147cc0b98c9fe709c
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-08 14:19:26 +0100

    upload file e inizio sviluppo progetto

---
+++ js/myscript.js
@@ -0,0 +1,23 @@
+/* Creazione di un biglietto del treno in base all'età dell'user e in base ai km che deve percorrere con un prezzo base di 0.21€/km se l'user ha meno di 18 anni allora bisogna applicare uno sconto del 20%, se invece a più di 65 anni bisogna applicare uno sconto del 40% */
+
+/*Nota bene il prezzo finale del biglietto non deve contenere più di 2 cifre decimali */
+
+// Inserisci il tuo nome
+
+// Inserisci il tuo cognome
+
+// Inserisci il tuo indirizzo
+
+// Inserisci il tuo numero di cellulare
+
+// Inserisci la tua età
+
+// Inserisci i km che devi percorrere
+
+// Calcolare il prezzo del biglietto in base ai dati inseriti dall'user
+
+// se l'età dell'user è >= a 18 e <= a 65 allora il prezzo per km è 0.21€/km
+
+// se l'età dell'user è < di 18 allora applica uno sconto del 20%
+
+// se l'età dell'user è > di 65 allora applica uno sconto del 40%
